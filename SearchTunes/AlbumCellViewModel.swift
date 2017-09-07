3156022dcae78505eb59688fc20b540291a81a06
albertojauregui <bastertwo@gmail.com>
2017-09-07 09:34:30 -0600

    Working on the AlbumListController with dataSource and viewModel

---
+++ SearchTunes/AlbumCellViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  AlbumCellViewModel.swift
+//  SearchTunes
+//
+//  Created by Alberto Jauregui on 06/09/17.
+//  Copyright © 2017 Alberto Jauregui. All rights reserved.
+//
+
+import Foundation
+
+struct AlbumCellViewModel {
+    let title: String
+    let releaseDate: String
+    let genre: String
+}
+
+extension AlbumCellViewModel {
+    init(album: Album) {
+        self.title = album.censoredName
+        self.genre = album.primaryGenre.name
+        
+        let formatter = DateFormatter()
+        formatter.locale = Locale.current
+        formatter.dateFormat = "MMM dd, yyyy"
+        
+        self.releaseDate = formatter.string(from: album.releaseDate)
+    }
+}
---
+++ SearchTunes/AlbumListController.swift
@@ -0,0 +1,56 @@
+//
+//  AlbumListController.swift
+//  SearchTunes
+//
+//  Created by Alberto Jauregui on 06/09/17.
+//  Copyright © 2017 Alberto Jauregui. All rights reserved.
+//
+
+import UIKit
+
+class AlbumListController: UITableViewController {
+    
+    private struct Constants {
+        static let AlbumCellHeight: CGFloat = 80
+    }
+    
+    var artist: Artist!
+    
+    lazy var dataSource: AlbumListDataSource = {
+        return AlbumListDataSource(albums: self.artist.albums)
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = artist.name
+        
+        tableView.dataSource = dataSource
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table View Delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return Constants.AlbumCellHeight
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showAlbum" {
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                let selectedAlbum = dataSource.album(at: selectedIndexPath)
+                
+                selectedAlbum.songs = Stub.songs
+                
+                let albumDetailController = segue.destination as! AlbumDetailController
+                albumDetailController.album = selectedAlbum
+            }
+        }
+    }
+
+}
