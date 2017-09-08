5ad051028c683c9e65a62c362e2eae40d42c90c7
albertojauregui <bastertwo@gmail.com>
2017-09-07 21:41:50 -0600

    Added JSONDownloader class and custom initializers with json

---
+++ SearchTunes/ItunesError.swift
@@ -0,0 +1,16 @@
+//
+//  ItunesError.swift
+//  SearchTunes
+//
+//  Created by Alberto Jauregui on 07/09/17.
+//  Copyright © 2017 Alberto Jauregui. All rights reserved.
+//
+
+import Foundation
+
+enum ItunesError: Error {
+    case requestFailed
+    case responseUnsuccessful
+    case invalidData
+    case jsonConversionFailure
+}
---
+++ SearchTunes/JSONDownloader.swift
@@ -0,0 +1,54 @@
+//
+//  JSONDownloader.swift
+//  SearchTunes
+//
+//  Created by Alberto Jauregui on 07/09/17.
+//  Copyright © 2017 Alberto Jauregui. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+class JSONDownloader {
+    let session: URLSession
+    
+    init(configuration: URLSessionConfiguration) {
+        self.session = URLSession(configuration: configuration)
+    }
+    
+    convenience init() {
+        self.init(configuration: .default)
+    }
+    
+    typealias JSON = [String: AnyObject]
+    typealias JSONTaskCompletionHandler = (JSON?, ItunesError?) -> Void
+    
+    func jsonTask(with request: URLRequest, completionHandler completion: @escaping JSONTaskCompletionHandler) -> URLSessionDataTask {
+        let task = session.dataTask(with: request) { data, response, error in
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(nil, .requestFailed)
+                return
+            }
+            
+            if httpResponse.statusCode == 200 {
+                if let data = data {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject]
+                        completion(json, nil)
+                    } catch {
+                        completion(nil, .jsonConversionFailure)
+                    }
+                } else {
+                    completion(nil, .invalidData)
+                }
+            } else {
+                completion(nil, .responseUnsuccessful)
+            }
+            
+        }
+        
+        return task
+    }
+}
