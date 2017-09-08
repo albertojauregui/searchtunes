5d2e6ef7d98e83eb022c3aa217c9120e97430785
albertojauregui <bastertwo@gmail.com>
2017-09-07 23:02:04 -0600

    Added the downloader for the artwork image

---
+++ SearchTunes/ArtworkDownloader.swift
@@ -0,0 +1,42 @@
+//
+//  ArtworkDownloader.swift
+//  SearchTunes
+//
+//  Created by Alberto Jauregui on 07/09/17.
+//  Copyright © 2017 Alberto Jauregui. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArtworkDownloader: Operation {
+    let album: Album
+    
+    init(album: Album) {
+        self.album = album
+        super.init()
+    }
+    
+    override func main() {
+        if self.isCancelled {
+            return
+        }
+        
+        guard let url = URL(string: album.artworkUrl) else {
+            return
+        }
+        
+        let imageData = try! Data(contentsOf: url)
+        
+        if self.isCancelled {
+            return
+        }
+        
+        if imageData.count > 0 {
+            album.artwork = UIImage(data: imageData)
+            album.artworkState = .downloaded
+        } else {
+            album.artworkState = .failed
+        }
+    }
+}
