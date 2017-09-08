8cff54ff884494d23fda22b8f87827a6b1951b11
albertojauregui <bastertwo@gmail.com>
2017-09-07 21:57:15 -0600

    Connection the APIClient to the searchController

--- SearchTunes/ViewController.swift
+++ SearchTunes/ViewController.swift
@@ -12,12 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let searchEndpoint = Itunes.search(term: "taylor swift ", media: .music(entity: .musicArtist, attribute: .artistTerm))
-        print(searchEndpoint.request)
-        
-        let lookupEndpoint = Itunes.lookup(id: 159260351, entity: MusicEntity.album)
-        print(lookupEndpoint.request)
     }
 
     override func didReceiveMemoryWarning() {
